994607c00ce72638b27a114a99765f00054612ea
Craig Dunn <craig.dunn@microsoft.com>
2020-12-03 12:31:34 -0800

    Hinge object

--- plugin-test/hello/www/js/index.js
+++ plugin-test/hello/www/js/index.js
@@ -49,20 +49,46 @@ function onResize() {
 
     window.ScreenHelper.getHinge(
         function(result) { 
-            document.getElementById('hinge').innerText = result; 
+            var h = new Hinge (result);
+            document.getElementById('hinge').innerText = 'Spanned: ' + h.isSpanned; 
 
-            if ('0,0,0,0' == result) {
-                document.getElementById('hinge').innerText = 'not spanned, no hinge';
-                document.getElementById('debughinge').classList.remove('spanned');
-                document.getElementById('debughinge').classList.add('single');
-            } else {
-                document.getElementById('hinge').innerText = 'hinge: ' + result;
+            if (h.isSpanned) {
+                document.getElementById('hinge').innerText += ' hinge: ' + h.toString();
                 document.getElementById('debughinge').classList.remove('single');
                 document.getElementById('debughinge').classList.add('spanned');
-            }
+
+                // document.getElementById('debughinge').setAttribute('left', h.x);
+                // document.getElementById('debughinge').setAttribute('top', h.y);
+            } else {
+                document.getElementById('debughinge').classList.remove('spanned');
+                document.getElementById('debughinge').classList.add('single');
+            } 
         },
-        function(error) {  document.getElementById('hinge').innerText = 'hinge: error ' + error; }
+        function(error) {  document.getElementById('hinge').innerText = ' hinge: error ' + error; }
     );
+}
+
+
+function Hinge(dimString) {
+    this.isSpanned = ('0,0,0,0' != dimString);
+
+    var arrayOfDims = dimString.split(',');
+    this.x = arrayOfDims[0] / 2.5;
+    this.y = arrayOfDims[1] / 2.5;
+    this.width = arrayOfDims[2] / 2.5;
+    this.height = arrayOfDims[3] / 2.5;
+
+    this.isVertical = (this.x > 0);
+    this.isHorizontal = (this.x == 0);
+}
+Hinge.prototype.toString = function() {
+    return this.x + ',' + this.y + '-' + this.width + ',' + this.height;
+}
+Hinge.prototype.setDimensions = function(div) {
+    div.style.left = this.x + 'px';
+    div.style.top = this.y + 'px';
+    div.style.width = this.width + 'px';
+    div.style.height = this.height + 'px';
 }
 
 function clickHandler() {
@@ -70,7 +96,17 @@ function clickHandler() {
     window.ScreenHelper.getHinge(
         function(result) { 
             // alert("hinge: " + result); 
-            alert('0,0,0,0' == result);
+            //alert('0,0,0,0' == result);
+            var h = new Hinge (result);
+            //alert('isSpanned: ' + h.isSpanned + ' ' + result);
+            //alert ('x is '+ h.x);
+            alert(h.x + 'px');
+            h.setDimensions(document.getElementById('debughinge'));
+            // document.getElementById('debughinge').style.left = h.x + 'px';
+            // document.getElementById('debughinge').style.top = h.y + 'px';
+            // document.getElementById('debughinge').style.width = h.width + 'px';
+            // document.getElementById('debughinge').style.height = h.height + 'px';
+            
         },
         function(error) { alert('error ' + error); }
     );
